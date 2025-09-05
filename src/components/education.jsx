319973cf72b69d59542644252abfc2a17bbe3c1b
Matt <50344355+mkrell23@users.noreply.github.com>
2025-09-05 15:36:11 -0400

    Basic education form

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,9 +1,35 @@
 
 
 export default function Education(){
+
+
+    function onSubmit(e){
+        e.preventDefault(); 
+        e.stopPropagation(); 
+    }
+
     return(
         <>
-        <h6>Hello Globe</h6>
+        <form onSubmit={ onSubmit }>
+            <h3>Education</h3>
+            <label>
+                School Attended: {' '}
+                <input type="text" name="school"/>
+            </label>{' '}
+            <label>
+                Date Started: {' '}
+                <input type="date" name="startDate"/>
+            </label>{' '}
+            <label>
+                Date Ended: {' '}
+                <input type="date" name="endDate"/>
+            </label>{' '}
+            <label>
+                Degree or Certificate Attained: {' '}
+                <input type="text" name="degree"/>
+            </label>{' '}
+            <button>Save</button>
+        </form>
         </>
     )
 }
