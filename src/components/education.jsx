be8db6e7de4c685d7b5540bff8a25b54203d17a0
Matt <50344355+mkrell23@users.noreply.github.com>
2025-09-05 16:21:23 -0400

    Controlling form elements and clearing on save

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,32 +1,54 @@
-
+import { useState } from "react";
 
 export default function Education(){
 
+    const [ school, setSchool ] = useState('')
+    const [ startDate, setStartDate ] = useState('')
+    const [ endDate, setEndDate ] = useState('')
+    const [ degree, setDegree ] = useState('')
 
     function onSubmit(e){
         e.preventDefault(); 
         e.stopPropagation(); 
+        setSchool('')
+        setStartDate('')
+        setEndDate('')
+        setDegree('')
     }
+
+//value="2008-12-21"
 
     return(
         <>
-        <form onSubmit={ onSubmit }>
+        <form  onSubmit={ onSubmit }>
             <h3>Education</h3>
             <label>
                 School Attended: {' '}
-                <input type="text" name="school"/>
+                <input type="text" name="school" 
+                    value={school}
+                    onChange={ e => setSchool(e.target.value)} 
+                />
             </label>{' '}
             <label>
                 Date Started: {' '}
-                <input type="date" name="startDate"/>
+                <input type="date" name="startDate"
+                    value={startDate}
+                    onChange={ e => setStartDate(e.target.value)}
+                />
             </label>{' '}
             <label>
                 Date Ended: {' '}
-                <input type="date" name="endDate"/>
+                <input type="date" name="endDate" 
+                    value={endDate}
+                    onChange={ e => setEndDate(e.target.value)}
+                />
             </label>{' '}
             <label>
                 Degree or Certificate Attained: {' '}
-                <input type="text" name="degree"/>
+                <input type="text" name="degree"
+                    value={degree}
+                    onChange={ e => setDegree(e.target.value)}
+                />
             </label>{' '}
             <button>Save</button>
         </form>
