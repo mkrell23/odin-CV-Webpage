2271de3685a12d58173a7626c393636c861a44f7
Matt <50344355+mkrell23@users.noreply.github.com>
2025-09-08 10:18:18 -0400

    Creation of experience section/component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@
 import './App.css'
 import Contact from './components/contact'
 import Education from './components/education'
+import Experience from './components/experience'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+    <hr />
       <Contact />
       <hr />
       <Education />
+      <hr />
+      <Experience />
     </>
   )
 }
---
+++ src/components/experience.jsx
@@ -0,0 +1,9 @@
+export default function Experience(){
+
+
+    return(
+        <>
+        <h4>Hey Globe</h4>
+        </>
+    )
+}
